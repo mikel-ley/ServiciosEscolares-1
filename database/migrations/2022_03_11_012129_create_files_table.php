00e27f62740b48682103543684deb28441f67766
ALBERTH GOMEZ <58703353+ALBERTH-CMD@users.noreply.github.com>
2022-03-29 16:16:51 -0600

    delete id user

--- database/migrations/2022_03_11_012129_create_files_table.php
+++ database/migrations/2022_03_11_012129_create_files_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('user_id'); // Relación con users
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
